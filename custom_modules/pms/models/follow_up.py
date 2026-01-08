36d97ad9f558030d6e066fb7775fdd7a8b788dec
fernandochoa13 <frosales1366@gmail.com>
2026-01-08 12:49:42 -0400

    account.follow.up references deleted from pms

--- custom_modules/pms/models/follow_up.py
+++ custom_modules/pms/models/follow_up.py
@@ -378,34 +378,6 @@ class ResPartner(models.Model):
             'res_id': self.id,
         }
 
-    def send_followup_email(self, options):
-        """
-        Send a follow-up report by email to customers in self
-        """
-        for record in self:
-            options['partner_id'] = record.id
-            self.env['account.followup.report']._send_email(options)
-
-    def send_followup_sms(self, options):
-        """
-        Send a follow-up report by sms to customers in self
-        """
-        for partner in self:
-            options['partner_id'] = partner.id
-            self.env['account.followup.report']._send_sms(options)
-
-    def get_followup_html(self, options=None):
-        """
-        Return the content of the follow-up report in HTML
-        """
-        if options is None:
-            options = {}
-        options.update({
-            'partner_id': self.id,
-            'followup_line_id': self.followup_line_id,
-            'keep_summary': True
-        })
-        return self.env['account.followup.report'].with_context(print_mode=True, lang=self.lang or self.env.user.lang).get_followup_report_html(options)
 
     def _get_followup_lines_info(self):
         """ returns the followup plan of the current user's allowed companies
@@ -552,11 +524,7 @@ class ResPartner(models.Model):
         Can be overridden to include more ways of sending the follow-up.
         """
         self.ensure_one()
-        followup_line = options.get('followup_line')
-        if options.get('email', followup_line.send_email):
-            self.send_followup_email(options)
-        if options.get('sms', followup_line.send_sms):
-            self.send_followup_sms(options)
+        # Follow-up email/sms functionality removed (account.followup.report not available)
 
     def _execute_followup_partner(self, options=None):
         """ Execute the actions to do with follow-ups for this partner (apart from printing).
@@ -608,9 +576,7 @@ class ResPartner(models.Model):
             - 'manual_followup': boolean to indicate whether this followup is triggered via the manual reminder wizard
         """
         self.ensure_one()
-        to_print = self._execute_followup_partner(options=options)
-        if options.get('print') and to_print:
-            return self.env['account.followup.report']._print_followup_letter(self, options)
+        self._execute_followup_partner(options=options)
 
     def _cron_execute_followup_company(self):
         followup_data = self._query_followup_data(all_partners=True)
@@ -631,150 +597,3 @@ class ResPartner(models.Model):
             self.env.cr.execute("DROP TABLE IF EXISTS followup_data_cache")
             self.with_context(allowed_company_ids=company.ids)._cron_execute_followup_company()
 
-
-class AccountFollowupReport(models.AbstractModel):
-    _inherit = 'account.followup.report'
-    _description = "Follow-up Report"
-
-    def _get_followup_report_lines(self, options):
-        """
-        Compute and return the lines of the columns of the follow-ups report.
-        """
-        # Get date format for the lang
-        partner = options.get('partner_id') and self.env['res.partner'].browse(options['partner_id']) or False
-        if not partner:
-            return []
-
-        lang_code = partner.lang
-        lines = []
-        res = {}
-        today = fields.Date.today()
-        line_num = 0
-        for l in partner.unreconciled_aml_ids.sorted().filtered(lambda aml: not aml.currency_id.is_zero(aml.amount_residual_currency)):
-            if l.company_id == self.env.company and not l.blocked:
-                currency = l.currency_id or l.company_id.currency_id
-                if currency not in res:
-                    res[currency] = []
-                res[currency].append(l)
-        for currency, aml_recs in res.items():
-            total = 0
-            total_issued = 0
-            for aml in aml_recs:
-                amount = aml.amount_residual_currency if aml.currency_id else aml.amount_residual
-                invoice_date = {
-                    'name': format_date(self.env, aml.move_id.invoice_date or aml.date, lang_code=lang_code),
-                    'class': 'date',
-                    'style': 'white-space:nowrap;text-align:center;',
-                    'template': 'account_followup.cell_template_followup_report',
-                }
-                date_due = format_date(self.env, aml.date_maturity or aml.move_id.invoice_date or aml.date, lang_code=lang_code)
-                total += not aml.blocked and amount or 0
-                is_overdue = today > aml.date_maturity if aml.date_maturity else today > aml.date
-                is_payment = aml.payment_id
-                if is_overdue or is_payment:
-                    total_issued += not aml.blocked and amount or 0
-                date_due = {
-                    'name': date_due, 'class': 'date',
-                    'style': 'white-space:nowrap;text-align:center;',
-                    'template': 'account_followup.cell_template_followup_report',
-                }
-                if is_overdue:
-                    date_due['style'] += 'color: red;'
-                if is_payment:
-                    date_due = ''
-                move_line_name = {
-                    'name': self._followup_report_format_aml_name(aml.name, aml.move_id.ref),
-                    'style': 'text-align:right; white-space:normal;',
-                    'template': 'account_followup.cell_template_followup_report',
-                }
-                amount = {
-                    'name': formatLang(self.env, amount, currency_obj=currency),
-                    'style': 'text-align:right; white-space:normal;',
-                    'template': 'account_followup.cell_template_followup_report',
-                }
-                line_num += 1
-                invoice_origin = aml.move_id.invoice_origin or ''
-                if len(invoice_origin) > 43:
-                    invoice_origin = invoice_origin[:40] + '...'
-                invoice_origin = {
-                    'name': invoice_origin,
-                    'style': 'text-align:center; white-space:normal;',
-                    'template': 'account_followup.cell_template_followup_report',
-                }
-                columns = [
-                    invoice_date,
-                    date_due,
-                    invoice_origin,
-                    move_line_name,
-                    amount,
-                ]
-                lines.append({
-                    'id': aml.id,
-                    'account_move': aml.move_id,
-                    'name': aml.move_id.name,
-                    'move_id': aml.move_id.id,
-                    'type': is_payment and 'payment' or 'unreconciled_aml',
-                    'unfoldable': False,
-                    'columns': [isinstance(v, dict) and v or {'name': v, 'template': 'account_followup.cell_template_followup_report'} for v in columns],
-                })
-            total_due = formatLang(self.env, total, currency_obj=currency)
-            line_num += 1
-
-            cols = \
-                [{
-                    'name': v,
-                    'template': 'account_followup.cell_template_followup_report',
-                } for v in [''] * 3] + \
-                [{
-                    'name': v,
-                    'style': 'text-align:right; white-space:normal;',
-                    'template': 'account_followup.cell_template_followup_report',
-                } for v in [total >= 0 and _('Total Due') or '', total_due]]
-
-            lines.append({
-                'id': line_num,
-                'name': '',
-                'class': 'total',
-                'style': 'border-top-style: double',
-                'unfoldable': False,
-                'level': 3,
-                'columns': cols,
-            })
-            if total_issued > 0:
-                total_issued = formatLang(self.env, total_issued, currency_obj=currency)
-                line_num += 1
-
-                cols = \
-                    [{
-                        'name': v,
-                        'template': 'account_followup.cell_template_followup_report',
-                    } for v in [''] * 3] + \
-                    [{
-                        'name': v,
-                        'style': 'text-align:right; white-space:normal;',
-                        'template': 'account_followup.cell_template_followup_report',
-                    } for v in [_('Total Overdue'), total_issued]]
-
-                lines.append({
-                    'id': line_num,
-                    'name': '',
-                    'class': 'total',
-                    'unfoldable': False,
-                    'level': 3,
-                    'columns': cols,
-                })
-            # Add an empty line after the total to make a space between two currencies
-            line_num += 1
-            lines.append({
-                'id': line_num,
-                'name': '',
-                'class': '',
-                'style': 'border-bottom-style: none',
-                'unfoldable': False,
-                'level': 0,
-                'columns': [{'template': 'account_followup.cell_template_followup_report'} for col in columns],
-            })
-        # Remove the last empty line
-        if lines:
-            lines.pop()
-        return lines

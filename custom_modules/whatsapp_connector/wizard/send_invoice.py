593d2304ac763c9e43bead349f65fd77cb9b95d7
fernandochoa13 <frosales1366@gmail.com>
2026-01-15 02:23:41 -0400

    Fix whatsapp_connector for Odoo 18: replace account.move.send inheritance with standalone wizard

--- custom_modules/whatsapp_connector/wizard/send_invoice.py
+++ custom_modules/whatsapp_connector/wizard/send_invoice.py
@@ -1,46 +1,29 @@
-from odoo import models, fields
-from odoo.tools.misc import get_lang
+from odoo import models, fields, api
 
-class SendInvoice(models.TransientModel):
-    _inherit = 'account.move.send'
 
+class SendInvoiceWhatsApp(models.TransientModel):
+    _name = 'account.move.send.whatsapp'
+    _description = 'Send Invoice via WhatsApp'
+
+    move_ids = fields.Many2many('account.move', string='Invoices')
     send_whatsapp = fields.Boolean(string='Send WhatsApp Message', default=True)
     whatsapp_body = fields.Text(string='WhatsApp Body')
 
-    def _send_whatsapp_message(self, invoice):
-        if self.send_whatsapp:
-            url = self.env['whatsapp.connector'].search([], limit=1).url
-            invoice.partner_id._send_wa_message(self.whatsapp_body, invoice.partner_id.id, url)
+    @api.model
+    def default_get(self, fields_list):
+        res = super().default_get(fields_list)
+        if self.env.context.get('active_model') == 'account.move':
+            res['move_ids'] = [(6, 0, self.env.context.get('active_ids', []))]
+        return res
 
-    
-    def send_and_print_action(self):
+    def _send_whatsapp_message(self, invoice):
+        if self.send_whatsapp and self.whatsapp_body:
+            url = self.env['whatsapp.connector'].search([], limit=1).url
+            if url and invoice.partner_id:
+                invoice.partner_id._send_wa_message(self.whatsapp_body, invoice.partner_id.id, url)
+
+    def action_send_whatsapp(self):
         self.ensure_one()
-        # Send the mails in the correct language by splitting the ids per lang.
-        # This should ideally be fixed in mail_compose_message, so when a fix is made there this whole commit should be reverted.
-        # basically self.body (which could be manually edited) extracts self.template_id,
-        # which is then not translated for each customer.
-        if self.composition_mode == 'mass_mail' and self.template_id:
-            active_ids = self.env.context.get('active_ids', self.res_id)
-            active_records = self.env[self.model].browse(active_ids)
-            langs = set(active_records.mapped('partner_id.lang'))
-            for lang in langs:
-                active_ids_lang = active_records.filtered(lambda r: r.partner_id.lang == lang).ids
-                self_lang = self.with_context(active_ids=active_ids_lang, lang=get_lang(self.env, lang).code)
-                self_lang.onchange_template_id()
-                self_lang._send_email()
-        else:
-            active_record = self.env[self.model].browse(self.res_id)
-            lang = get_lang(self.env, active_record.partner_id.lang).code
-            self.with_context(lang=lang)._send_email()
-        
-        if self.send_whatsapp:
-                active_ids = self.env.context.get('active_ids', self.res_id)
-                active_records = self.env[self.model].browse(active_ids)
-                for invoice in active_records:
-                    self._send_whatsapp_message(invoice)
-        
-        
-        if self.is_print:
-            return self._print_document()
-        
+        for invoice in self.move_ids:
+            self._send_whatsapp_message(invoice)
         return {'type': 'ir.actions.act_window_close'}
